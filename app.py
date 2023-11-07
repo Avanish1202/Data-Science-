aaade122f51362593725929e3e8d8a764855c84a
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-08 00:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ import io
 
 # Function to fetch the movie poster
 def fetch_poster(movie_id):
-    # Your fetch_poster function code here
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=YOUR_API_KEY&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500" + poster_path
+    return full_path
 
 # Function to recommend movies
 def recommend(movie):
-    # Your recommend function code here
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # Fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    return recommended_movie_names, recommended_movie_posters
 
 # Streamlit app
 st.header('Movie Recommender System')
