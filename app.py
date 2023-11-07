7922ef56e32eab42a62e618ef113cf0e98755265
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-08 00:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
 import pickle
 import streamlit as st
 import requests
+import io
 
 # Function to fetch the movie poster
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=YOUR_API_KEY&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500" + poster_path
-    return full_path
+    # Your fetch_poster function code here
 
 # Function to recommend movies
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # Fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-    return recommended_movie_names, recommended_movie_posters
+    # Your recommend function code here
 
 # Streamlit app
 st.header('Movie Recommender System')
 
 # Download the pickle file from GitHub
 pickle_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/movie_list.pkl'
-movies = pickle.load(requests.get(pickle_url, stream=True).raw)
+response = requests.get(pickle_url)
+if response.status_code == 200:
+    # Load the data using the io module
+    movies = pickle.load(io.BytesIO(response.content))
+else:
+    st.error("Failed to load movie data. Please check the URL.")
 
 # Download the similarity pickle file from GitHub
 similarity_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/similarity.pkl'
-similarity = pickle.load(requests.get(similarity_url, stream=True).raw)
+response = requests.get(similarity_url)
+if response.status_code == 200:
+    # Load the data using the io module
+    similarity = pickle.load(io.BytesIO(response.content))
+else:
+    st.error("Failed to load similarity data. Please check the URL.")
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
