55f015a4603e8ae53c80d4af0bce6855499e05ac
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-08 00:23:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import streamlit as st
 import requests
 import io
 
+# Define the URLs for movie data and similarity data
+pickle_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/movie_list.pkl'
+similarity_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/similarity.pkl'
+
+# Function to fetch the movie poster
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY&language=en-US"
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500" + poster_path
+    return full_path
+
 # Function to recommend movies
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -19,14 +32,12 @@ def recommend(movie):
 # Streamlit app
 st.header('Movie Recommender System')
 
-# Download the pickle file from GitHub
-pickle_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/movie_list.pkl'
+# Attempt to load the movie data from the URL
 response = requests.get(pickle_url)
 if response.status_code == 200:
     # Load the data using the io module
     movies = pickle.load(io.BytesIO(response.content))
-    # Load the similarity data
-    similarity_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/similarity.pkl'
+    # Attempt to load the similarity data from the URL
     response = requests.get(similarity_url)
     if response.status_code == 200:
         similarity = pickle.load(io.BytesIO(response.content))
