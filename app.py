188928625ff8956e3e484fe90d95c29e3dc1da9e
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-08 00:28:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,68 +3,54 @@ import streamlit as st
 import requests
 import io
 
-# Define the URLs for movie data and similarity data
-movie_data_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/movie_list.pkl'
-similarity_data_url = 'https://github.com/Avanish1202/Data-Science-/raw/main/similarity.pkl'
-
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # Fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names, recommended_movie_posters
-
-st.header('Movie Recommender System')
-
-# Attempt to load the movie data from the URL
-response = requests.get(movie_data_url)
-if response.status_code == 200:
-    movies = pickle.load(io.BytesIO(response.content))
-    # Attempt to load the similarity data from the URL
-    response = requests.get(similarity_data_url)
+# Function to fetch data from a URL
+def fetch_data_from_url(url):
+    response = requests.get(url)
     if response.status_code == 200:
-        similarity = pickle.load(io.BytesIO(response.content))
+        return response.content
     else:
-        st.error("Failed to load similarity data. Please check the URL.")
-else:
-    st.error("Failed to load movie data. Please check the URL.")
+        st.error(f"Failed to load data from URL: {url}")
+        return None
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+# Specify the URLs for movie data and similarity data
+movie_data_url = 'https://raw.githubusercontent.com/Avanish1202/Data-Science-/main/movie_list.pkl'
+similarity_data_url = 'https://raw.githubusercontent.com/Avanish1202/Data-Science-/main/similarity.pkl'
 
-if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+# Load movie data
+movie_data = fetch_data_from_url(movie_data_url)
+if movie_data:
+    movies = pickle.load(io.BytesIO(movie_data))
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+# Load similarity data
+similarity_data = fetch_data_from_url(similarity_data_url)
+if similarity_data:
+    similarity = pickle.load(io.BytesIO(similarity_data))
+
+# Define the Streamlit app
+def main():
+    st.header('Movie Recommender System')
+    movie_list = movies['title'].values
+    selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+    if st.button('Show Recommendation'):
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+        col1, col2, col3, col4, col5 = st.beta_columns(5)
+        with col1:
+            st.text(recommended_movie_names[0])
+            st.image(recommended_movie_posters[0])
+        with col2:
+            st.text(recommended_movie_names[1])
+            st.image(recommended_movie_posters[1])
+        with col3:
+            st.text(recommended_movie_names[2])
+            st.image(recommended_movie_posters[2])
+        with col4:
+            st.text(recommended_movie_names[3])
+            st.image(recommended_movie_posters[3])
+        with col5:
+            st.text(recommended_movie_names[4])
+            st.image(recommended_movie_posters[4])
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    main()
