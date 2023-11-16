8f76c1a7be3743242443b62a0c575d089e86a456
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 19:49:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 import pickle
 import streamlit as st
-import requests
 import io
 
-# Function to fetch data from a URL
-def fetch_data_from_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        st.error(f"Failed to load data from URL: {url}")
+# Function to fetch data from a local file
+def fetch_data_from_file(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            return file.read()
+    except Exception as e:
+        st.error(f"Failed to load data from file: {file_path}\nError: {e}")
         return None
 
-# Specify the URLs for movie data and similarity data
-movie_data_url = 'movie_list.pkl'
-similarity_data_url = 'similarity.pkl'
+# Specify the file paths for movie data and similarity data
+movie_data_path = 'movie_list.pkl'
+similarity_data_path = 'similarity.pkl'
 
 # Load movie data
-movie_data = fetch_data_from_url(movie_data_url)
+movie_data = fetch_data_from_file(movie_data_path)
 if movie_data:
     movies = pickle.load(io.BytesIO(movie_data))
 
 # Load similarity data
-similarity_data = fetch_data_from_url(similarity_data_url)
+similarity_data = fetch_data_from_file(similarity_data_path)
 if similarity_data:
     similarity = pickle.load(io.BytesIO(similarity_data))
+
 
 # Define the Streamlit app
 def main():
